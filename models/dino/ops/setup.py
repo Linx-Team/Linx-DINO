73439311e3e0932c22fb50ca777b6e1d78d8d02d
keanu-paik <yspaik@gmail.com>
2022-07-13 20:22:45 +0900

    ops compile 수정

--- models/dino/ops/setup.py
+++ models/dino/ops/setup.py
@@ -46,7 +46,8 @@ def get_extensions():
             "-D__CUDA_NO_HALF2_OPERATORS__",
         ]
     else:
-        raise NotImplementedError('Cuda is not availabel')
+        # raise NotImplementedError('Cuda is not availabel')
+        print("Cuda is not available. Use CPU instead")
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
     include_dirs = [extensions_dir]
